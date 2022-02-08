1960e22f50ded309bed4e4788409a772a89e9615
David Hakki <david.hakki@elitetechnicalcare.com>
2022-02-08 06:34:47 -0500

    Initial Commit

---
+++ ParseSwift Example/ParseSwift_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParseSwift_ExampleApp.swift
+//  ParseSwift Example
+//
+//  Created by David Hakki on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ParseSwift_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
